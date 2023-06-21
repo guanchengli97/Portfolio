a152d13e2e701eba5f7f04a2ada7f489b4789a22
liguancheng <1135962690@qq.com>
2023-06-21 15:24:35 -0500

    update links

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,11 +10,11 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
             from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            <br /> I am a final year student pursuing an  MSc (Master Computer Science)
+            in Computer Science at Rice University.
             <br />
             Additionally, I am currently employed as a software developer at
-            Juspay.
+            Lenwell international Corp.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Guancheng</footer>
         </blockquote>
       </Card.Body>
     </Card>
