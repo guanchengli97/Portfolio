919d29a87a7eb19b91df9ac77def5caa2a41ce2b
liguancheng <1135962690@qq.com>
2023-07-12 21:54:34 -0500

    add my resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Guancheng_s_Resume_3.pdf";
+// import pdf from "../../Assets/Guancheng_s_Resume_3.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+const pdf = "https://imagebucket97.s3.us-east-2.amazonaws.com/Guancheng_s_Resume_3.pdf";
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
